9b081562aaa379c1a074af31bc94a2e655779307
Alertyz <alec_meier@hotmail.com>
2024-04-15 01:56:02 -0300

    Bagunça_2

---
+++ dic.py
@@ -0,0 +1,199 @@
+colunas_dic = [
+    'Q001', 'Q002', 'Q003', 'Q004', 'Q005', 'Q006', 'Q007', 'Q008', 'Q009', 
+    'Q010', 'Q011', 'Q012', 'Q013', 'Q014', 'Q015', 'Q016', 'Q017', 'Q018', 
+    'Q019', 'Q020', 'Q021', 'Q022', 'Q023', 'Q024', 'Q025'
+]
+
+q001_dict = {
+    'A': 1,  # Nunca estudou.
+    'B': 1,  # Não completou a 4ª série/5º ano do Ensino Fundamental.
+    'C': 2,  # Completou a 4ª série/5º ano, mas não completou a 8ª série/9º ano do Ensino Fundamental.
+    'D': 2,  # Completou a 8ª série/9º ano do Ensino Fundamental, mas não completou o Ensino Médio.
+    'E': 3,  # Completou o Ensino Médio, mas não completou a Faculdade.
+    'F': 3,  # Completou a Faculdade, mas não completou a Pós-graduação.
+    'G': 3  # Completou a Pós-graduação.
+}
+q002_dict = {
+    'A': 1,  # Nunca estudou.
+    'B': 1,  # Não completou a 4ª série/5º ano do Ensino Fundamental.
+    'C': 2,  # Completou a 4ª série/5º ano, mas não completou a 8ª série/9º ano do Ensino Fundamental.
+    'D': 2,  # Completou a 8ª série/9º ano do Ensino Fundamental, mas não completou o Ensino Médio.
+    'E': 2,  # Completou o Ensino Médio, mas não completou a Faculdade.
+    'F': 3,  # Completou a Faculdade, mas não completou a Pós-graduação.
+    'G': 3  # Completou a Pós-graduação.
+}
+q003_dict = {
+    'A': 1,  # Grupo 1: Lavrador, agricultor sem empregados, bóia fria, criador de animais etc.
+    'B': 2,  # Grupo 2: Diarista, empregado doméstico, cuidador de idosos, babá, cozinheiro etc.
+    'C': 3,  # Grupo 3: Padeiro, cozinheiro industrial ou em restaurantes, sapateiro, costureiro etc.
+    'D': 4,  # Grupo 4: Professor, técnico, policial, militar de baixa patente, corretor de imóveis etc.
+    'E': 5,  # Grupo 5: Médico, engenheiro, dentista, psicólogo, economista, advogado etc.
+    'F': 6   # Não sei.
+}
+q004_dict = {
+    'A': 1,  # Grupo 1: Lavradora, agricultora sem empregados, bóia fria, criadora de animais etc.
+    'B': 2,  # Grupo 2: Diarista, empregada doméstica, cuidadora de idosos, babá, cozinheira etc.
+    'C': 3,  # Grupo 3: Padeira, cozinheira industrial ou em restaurantes, sapateira, costureira etc.
+    'D': 4,  # Grupo 4: Professora, técnica, policial, militar de baixa patente, corretora de imóveis etc.
+    'E': 5,  # Grupo 5: Médica, engenheira, dentista, psicóloga, economista, advogada etc.
+    'F': 6   # Não sei.
+}
+q005_dict = {
+    1: 1,   # 1, pois moro sozinho(a).
+    2: 2,   # 2
+    3: 3,   # 3
+    4: 4,   # 4
+    5: 4,   # 5
+    6: 4,   # 6
+    7: 4,   # 7
+    8: 4,   # 8
+    9: 4,   # 9
+    10: 4, # 10
+    11: 4, # 11
+    12: 4, # 12
+    13: 4, # 13
+    14: 4, # 14
+    15: 4, # 15
+    16: 4, # 16
+    17: 4, # 17
+    18: 4, # 18
+    19: 4, # 19
+    20: 4  # 20
+}
+q006_dict = {
+    'A': 1,  # Nenhuma Renda
+    'B': 2,  # Até R$ 1.212,00
+    'C': 2,  # De R$ 1.212,01 até R$ 1.818,00.
+    'D': 2,  # De R$ 1.818,01 até R$ 2.424,00.
+    'E': 2,  # De R$ 2.424,01 até R$ 3.030,00.
+    'F': 3,  # De R$ 3.030,01 até R$ 3.636,00.
+    'G': 3,  # De R$ 3.636,01 até R$ 4.848,00.
+    'H': 3,  # De R$ 4.848,01 até R$ 6.060,00.
+    'I': 3,  # De R$ 6.060,01 até R$ 7.272,00.
+    'J': 4,  # De R$ 7.272,01 até R$ 8.484,00.
+    'K': 4,  # De R$ 8.484,01 até R$ 9.696,00.
+    'L': 4,  # De R$ 9.696,01 até R$ 10.908,00.
+    'M': 4,  # De R$ 10.908,01 até R$ 12.120,00.
+    'N': 5,  # De R$ 12.120,01 até R$ 14.544,00.
+    'O': 5,  # De R$ 14.544,01 até R$ 18.180,00.
+    'P': 5,  # De R$ 18.180,01 até R$ 24.240,00.
+    'Q': 5   # Acima de R$ 24.240,00.
+}
+q007_dict = {
+    'A': 1,  # Não.
+    'B': 2,  # Sim, um ou dois dias por semana.
+    'C': 3,  # Sim, três ou quatro dias por semana.
+    'D': 3   # Sim, pelo menos cinco dias por semana.
+}
+q008_dict = {
+    'A': 1,  # Não.
+    'B': 2,  # Sim, uma.
+    'C': 3,  # Sim, duas.
+    'D': 3,  # Sim, três.
+    'E': 3   # Sim, quatro ou mais.
+}
+q009_dict = {
+    'A': 1,  # Não.
+    'B': 2,  # Sim, uma.
+    'C': 3,  # Sim, duas.
+    'D': 3,  # Sim, três.
+    'E': 3   # Sim, quatro ou mais.
+}
+q010_dict = {
+    'A': 1,  # Não.
+    'B': 2,  # Sim, uma.
+    'C': 3,  # Sim, duas.
+    'D': 3,  # Sim, três.
+    'E': 3   # Sim, quatro ou mais.
+}
+q011_dict = {
+    'A': 1,  # Não.
+    'B': 2,  # Sim, uma.
+    'C': 3,  # Sim, duas.
+    'D': 3,  # Sim, três.
+    'E': 3   # Sim, quatro ou mais.
+}
+q012_dict = {
+    'A': 1,  # Não.
+    'B': 2,  # Sim, uma.
+    'C': 3,  # Sim, duas.
+    'D': 3,  # Sim, três.
+    'E': 3   # Sim, quatro ou mais.
+}
+q013_dict = {
+    'A': 1,  # Não.
+    'B': 2,  # Sim, uma.
+    'C': 3,  # Sim, duas.
+    'D': 3,  # Sim, três.
+    'E': 3   # Sim, quatro ou mais.
+}
+q014_dict = {
+    'A': 1,  # Não.
+    'B': 2,  # Sim, uma.
+    'C': 3,  # Sim, duas.
+    'D': 3,  # Sim, três.
+    'E': 3   # Sim, quatro ou mais.
+}
+q015_dict = {
+    'A': 1,  # Não.
+    'B': 2,  # Sim, uma.
+    'C': 3,  # Sim, duas.
+    'D': 3,  # Sim, três.
+    'E': 3   # Sim, quatro ou mais.
+}
+q016_dict = {
+    'A': 1,  # Não.
+    'B': 2,  # Sim, uma.
+    'C': 3,  # Sim, duas.
+    'D': 3,  # Sim, três.
+    'E': 3   # Sim, quatro ou mais.
+}
+q017_dict = {
+    'A': 1,  # Não.
+    'B': 2,  # Sim, uma.
+    'C': 3,  # Sim, duas.
+    'D': 3,  # Sim, três.
+    'E': 3   # Sim, quatro ou mais.
+}
+q018_dict = {
+    'A': 0,  # Não.
+    'B': 1   # Sim.
+}
+q019_dict = {
+    'A': 1,  # Não.
+    'B': 2,  # Sim, uma.
+    'C': 3,  # Sim, duas.
+    'D': 3,  # Sim, três.
+    'E': 3   # Sim, quatro ou mais.
+}
+q020_dict = {
+    'A': 0,  # Não.
+    'B': 1   # Sim.
+}
+q021_dict = {
+    'A': 0,  # Não.
+    'B': 1   # Sim.
+}
+q022_dict = {
+    'A': 1,  # Não.
+    'B': 2,  # Sim, um.
+    'C': 3,  # Sim, dois.
+    'D': 3,  # Sim, três.
+    'E': 3   # Sim, quatro ou mais.
+}
+q023_dict = {
+    'A': 0,  # Não.
+    'B': 1   # Sim.
+}
+q024_dict = {
+    'A': 1,  # Não.
+    'B': 2,  # Sim, um.
+    'C': 3,  # Sim, dois.
+    'D': 3,  # Sim, três.
+    'E': 3   # Sim, quatro ou mais.
+}
+q025_dict = {
+    'A': 0,  # Não.
+    'B': 1   # Sim.
+}
+
